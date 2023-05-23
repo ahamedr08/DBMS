3aa5d1646045fbf018c4c4c5bcd9a1a47ae31a34
Ahamed R <134275920+ahamedr08@users.noreply.github.com>
2023-05-23 10:27:35 +0530

    sql

--- Query4.sql
+++ Query4.sql
@@ -7,8 +7,8 @@ Query : Find the number of OD availed by the specified staff.
 Procedure : To list the staff members who have availed more leave( including all leave).
 -------------------------------------------------------------------------
 
--- Create Staff2 table
-CREATE TABLE Staff2(
+-- Create Staff table
+CREATE TABLE Staff(
 Staff_id INT,
 Staff_Name VARCHAR(50),
 PRIMARY KEY (Staff_id)
@@ -24,27 +24,27 @@ FOREIGN KEY (Staff_id) REFERENCES Staff(Staff_id)
 );
 
 -- Insert sample records into Staff table
-INSERT INTO Staff2 VALUES (1, 'John');
-INSERT INTO Staff2 VALUES (2, 'Alice');
-INSERT INTO Staff2 VALUES (3, 'Mike');
-select * from staff2;
+INSERT INTO Staff VALUES (1, 'John');
+INSERT INTO Staff VALUES (2, 'Alice');
+INSERT INTO Staff VALUES (3, 'Mike');
+select * from staff;
 
 -- Insert sample records into Leave_Avail table
-INSERT INTO Leave_Avail2 VALUES (1, '07-08-2023', 1,'CL');
-INSERT INTO Leave_Avail2 VALUES (1, '09-08-2023',2, 'CL');
-INSERT INTO Leave_Avail2 VALUES (1, '02-01-2023', 1,'ML');
-INSERT INTO Leave_Avail2 VALUES (1, '08-09-2023', 1,'ML');
-INSERT INTO Leave_Avail2 VALUES (1, '01-01-2023', 1,'ML');
-INSERT INTO Leave_Avail2 VALUES (1, '03-04-2023',1, 'OD');
-INSERT INTO Leave_Avail2 VALUES (2, '09-04-2023',1, 'CL');
-INSERT INTO Leave_Avail2 VALUES (2, '02-04-2023',1, 'OD');
-INSERT INTO Leave_Avail2 VALUES (3, '09-09-2023', 1,'ML');
-select * from Leave_Avail2;
+INSERT INTO Leave_Avail VALUES (1, '07-08-2023', 1,'CL');
+INSERT INTO Leave_Avail VALUES (1, '09-08-2023',2, 'CL');
+INSERT INTO Leave_Avail VALUES (1, '02-01-2023', 1,'ML');
+INSERT INTO Leave_Avail VALUES (1, '08-09-2023', 1,'ML');
+INSERT INTO Leave_Avail VALUES (1, '01-01-2023', 1,'ML');
+INSERT INTO Leave_Avail VALUES (1, '03-04-2023',1, 'OD');
+INSERT INTO Leave_Avail VALUES (2, '09-04-2023',1, 'CL');
+INSERT INTO Leave_Avail VALUES (2, '02-04-2023',1, 'OD');
+INSERT INTO Leave_Avail VALUES (3, '09-09-2023', 1,'ML');
+select * from Leave_Avail;
 
 -------------------------------QUERY---------------------------------
 
 SELECT SUM(No_Days) AS Total_OD_Availed
-FROM Leave_Avail2
+FROM Leave_Avail
 WHERE Staff_id = '1'
 AND Type = 'OD';
 
@@ -57,7 +57,7 @@ BEGIN
 FOR staff_rec IN (
 SELECT s.Staff_id, s.Staff_Name, COUNT(l.Type) AS Total_Leaves
 FROM Staff s
-JOIN Leave_Avail2 l ON s.Staff_id = l.Staff_id
+JOIN Leave_Avail l ON s.Staff_id = l.Staff_id
 GROUP BY s.Staff_id, s.Staff_Name
 ORDER BY Total_Leaves DESC
 )
